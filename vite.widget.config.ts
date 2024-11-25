ed46150a8df8c7fc08e9e01cf715d691dbe0f28d
B3tMy3x <97553064+B3tMy3x@users.noreply.github.com>
2024-11-25 19:58:51 +0300

    2

--- vite.widget.config.ts
+++ vite.widget.config.ts
@@ -1,21 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/widget/',
   build: {
     lib: {
-      entry: 'src/widget/index.ts',
+      entry: './src/widget.ts',
       name: 'ChatbotWidget',
-      formats: ['umd'],
-      fileName: 'chatbot-widget'
+      fileName: 'chatbot-widget',
     },
-    outDir: 'dist/widget',
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    }
-  }
+        globals: {
+          'chatbot-widget': 'ChatbotWidget',
+        },
+      },
+    },
+  },
 });
